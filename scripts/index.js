9910fe10fd8a5e00ecd140a732cfcfdfc3ff7ec3
howdy39 <tatsuya.howdy@gmail.com>
2019-03-12 23:28:11 +0900

    fix: connect to gas

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,23 @@
 const lighthouse = require('./lighthouse.js');
+const request = require('request');
 const scorePromise = lighthouse.getLighthouseScore('https://techthetoaster.stores.jp/')
 scorePromise.then((score) => {
   console.log(score);
-  console.log(score.Performance);
+  const message = {
+    performance: score.Performance,
+    accessibility: score.Accessibility,
+    best_practices: score['Best Practices'],
+    seo: score.SEO,
+    progressive_web_app: score['Progressive Web App']
+  };
+
+  const options = {
+    uri: "https://script.google.com/macros/s/AKfycbyMw1wxBBGqYolnrTFTwGIx63DKBQSS-ZKvscZuJTgtthmN1CcI/exec",
+    headers: {
+      "Content-type": "application/json",
+    },
+    json: message
+  };
+
+  request.post(options, function(error, response, body){});
 });
